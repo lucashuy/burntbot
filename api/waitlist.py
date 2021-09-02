0c425c518ed77a636ff423c791b56eade1283032
Lucas <contact@itslucas.win>
2021-09-02 17:01:58 -0500

    Added gzip compression and counting swaps today

--- api/waitlist.py
+++ api/waitlist.py
@@ -4,10 +4,20 @@ import globals
 
 from api.exception import raise_exception
 from utilities.log import log
+from utilities.datetime import get_reset_datetime, string_to_datetime
 
-def get_waitlist():
+def update_waitlist():
+	'''
+	Update the local cache containing waitlist details (position, points, etc)
+	'''
+	
 	local_headers = globals.headers.copy()
+
+	# use etags to prevent redundant data transfer
 	if (globals.waitlist_etag): local_headers['If-None-Match'] = globals.waitlist_etag
+
+	# accept gzip compression (since those with lots of swaps have huge responses, > 3MB)
+	local_headers['Accept-Encoding'] = 'gzip, deflate'
 
 	response = requests.get('https://api.shakepay.com/card/waitlist', headers = local_headers)
 
@@ -28,6 +38,24 @@ def get_waitlist():
 		globals.waitlist_points = data['score']
 		globals.waitlist_position = data['rank']
 
+		# save paddles
 		globals.waitlist_paddles = []
 		for paddle in data['badges']:
 			globals.waitlist_paddles.append(paddle)
+
+		today_start_datetime = get_reset_datetime()
+		today_swaps = 0
+
+		# calculate the number of swaps made today
+		# data is ordered from most recent to oldest
+		for transaction in data['history']:
+			transaction_datetime = string_to_datetime(transaction['createdAt'])
+
+			if (transaction_datetime >= today_start_datetime):
+				# add swap if it was completed today
+				today_swaps = today_swaps + 1
+			else:
+				# swap is older, any data past this point is not needed
+				break
+
+		globals.waitlist_swaps = today_swaps
